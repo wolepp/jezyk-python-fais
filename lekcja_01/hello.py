e30374c8f9d4c89f21846d9962ff098cc17040b6
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-10-10 09:17:48 +0200

    Dodano hello.py w lekcji 01

---
+++ lekcja_01/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-2 -*-
+#
+# hello.py
+#
+# Pierwszy program w Pythonie.
+# W pierwszej linii mamy ścieżkę dostępu do interpretera.
+# W drugiej linii informacja o kodowaniu znaków w pliku.
+# Dzięki temu można w pliku stosować polskie znaki w napisach.
+# Po znaku '#' mamy komentarz do końca linii.
+
+print ( "Hello, world!" )
