5865d167cc0277093d001955b04301a714436aa5
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-10-27 22:44:58 +0100

    dodano funkcje

---
+++ lekcja_03/zad03.py
@@ -0,0 +1,27 @@
+def z3_1():
+    pass
+
+def z3_2():
+    pass
+
+def z3_3():
+    pass
+
+def z3_4():
+    pass
+
+def z3_5():
+    pass
+
+def z3_6():
+    pass
+
+def z3_8():
+    pass
+
+def z3_9():
+    pass
+
+def z3_10():
+    pass
+
