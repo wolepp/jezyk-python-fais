e9d7eba5fcaa98b708f53e429dc02588def1d823
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-10-28 21:58:54 +0100

    dodano rozwiazanie 3.2

--- lekcja_03/zad03.py
+++ lekcja_03/zad03.py
@@ -5,7 +5,7 @@ def z3_1():
         result = x;
     else:
         result = y;
-    print(result)
+    assert result == 3
 
     # Kod linijke nizej jest niepoprawny
     # for i in "qwerty": if ord(i) < 100: print i
@@ -20,7 +20,35 @@ def z3_1():
     for i in "axby": print(ord(i) if ord(i) < 100 else i)
 
 def z3_2():
-    pass
+    L = [3, 5, 4] ; L = L.sort()
+    # Powyzszy kod, dziala, jest semantycznie poprawny, ale dziala inaczej
+    # niz mozna podejrzewac. Metoda sort() klasy list sortuje ja i zwraca None,
+    # ktore jest nastepnie przypisane do L. Wywolujac type(L) otrzymujemy
+    # <class 'NoneType'>
+    assert type(L) == type(None)
+
+    # x, y = 1, 2, 3 
+    # Powyzszy kod jest niepoprawny ze wzgledu na inna ilosc zmiennych po
+    # lewej stronie przypisania i wyrazen po prawej. Wyjatek ValueError
+
+    # X = 1, 2, 3 ; X[1] = 4
+    # Pierwsze wyrazenie to przypisanie trzyelementowej tablicy do zmiennej X
+    # (jak najbardziej poprawne), natomiast drugie wyrazenie to proba przypisa-
+    # nia drugiemu elementowi krotki wartosci - tego zrobic sie nie da:
+    # zwracany jest wyjatek TypeError
+
+    # X = [1, 2, 3]; X[3] = 4
+    # Kod niepoprawny - X jest tablica, gdzie ostatni indeks to 2. Przypisanie
+    # powoduje wyjatek IndexError
+
+    # X = "abc" ; X.append("d")
+    # X jest stringiem, ktory nie ma metody append, wyjatek AttributeError
+    
+    # map(pow, range(8))
+    # Funkcja pow wymaga podania dwoch argumentow, a podany jest tylko jeden:
+    # range(8). Przykladowy dzialajacy kod:
+    mapowanie = map(pow, range(8), [2 for x in range(8)])
+    assert mapowanie == [0, 1, 4, 9, 16, 25, 36, 49]
 
 def z3_3():
     pass
