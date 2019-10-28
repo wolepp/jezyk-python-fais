57e2b479617c58f6130f3b11a303f439f555c676
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-10-28 21:12:23 +0100

    dodano rozwiązanie 3.1

--- lekcja_03/zad03.py
+++ lekcja_03/zad03.py
@@ -1,5 +1,23 @@
 def z3_1():
-    pass
+    # Ten kod jest poprawny skladniowo zarowno w python2 jak i python3
+    x = 2 ; y = 3 ;
+    if (x > y):
+        result = x;
+    else:
+        result = y;
+    print(result)
+
+    # Kod linijke nizej jest niepoprawny
+    # for i in "qwerty": if ord(i) < 100: print i
+    # Poprawny kod w python2: (gdzie print jest instrukcja)
+    for i in "qwerty":
+        if ord(i) < 100:
+            print(i)
+
+    # Kod poprawny w python2, w python3 trzeba odpowiednio zastosowac print().
+    # Poprawny, zakomentowany w celu kompatybilnosci
+    # for i in "axby": print ord(i) if ord(i) < 100 else i
+    for i in "axby": print(ord(i) if ord(i) < 100 else i)
 
 def z3_2():
     pass
@@ -25,3 +43,12 @@ def z3_9():
 def z3_10():
     pass
 
+z3_1();
+z3_2();
+z3_3();
+z3_4();
+z3_5();
+z3_6();
+z3_8();
+z3_9();
+z3_10();
