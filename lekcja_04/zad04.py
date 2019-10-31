b11317ba363e81c242c57fa8bf8a3ef64b4af87b
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-10-31 18:51:25 +0100

    przygotowano strukture pliku

---
+++ lekcja_04/zad04.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: iso-8859-2 -*-
+
+# TODO: sprawdzic kodowanie!
+
+def zad4_2(argu):
+    pass
+
+def zad4_3():
+    pass
+
+def zad4_4():
+    pass
+
+def zad4_5():
+    pass
+
+def zad4_6():
+    pass
+
+def zad4_7():
+    pass
+
+if __name__ == '__main__':
+    pass
