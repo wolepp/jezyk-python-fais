dd0b01e23e725596e439b942cc47551324649006
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-11-12 15:23:27 +0100

    dodano plik fracs.py z deklaracjami

---
+++ lekcja_05/fracs.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+"""
+Moduł z funkcjami do działań na ułamkach.
+Ułamek reprezentowany przez listę dwóch liczb całkowitych [licznik, mianownik].
+
+Wojciech Lepich
+"""
+
+import doctest
+
+
+def add_frac(frac1, frac2): pass
+
+
+def sub_frac(frac1, frac2): pass
+
+
+def mul_frac(frac1, frac2): pass
+
+
+def div_frac(frac1, frac2): pass
+
+
+def is_positive(frac): pass
+
+
+def is_zer(frac): pass
+
+
+def cmp_frac(frac1, frac2): pass
+
+
+def frac2float(frac): pass
