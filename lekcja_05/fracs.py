96fef08009ccfbb62e929d2a71ad38184069b615
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-11-12 19:30:39 +0100

    fracs: poprawiona funkcja is_frac, usunięte doctest. Doctest dostępne w gałęzi fracs_doctests

--- lekcja_05/fracs.py
+++ lekcja_05/fracs.py
@@ -13,20 +13,16 @@ from math import gcd    # fractions.gcd is deprecated
 
 def is_frac(*args):
     """
-    Sprawdza czy frac jest ułamkiem i zwraca True lub False.
-
-    >>> is_frac([1, 2])
-    True
-    >>> is_frac([1, 2], [-4, 3])
-    True
-    >>> is_frac([1, 2, 3])
-    False
-    >>> is_frac(5)
-    False
+    Sprawdza czy podane argumenty są ułamkami.
+    Zwraca False, jeżeli którykolwiek z argumentów nie jest ułamkiem.
+    Zwraca True w przeciwnym wypadku.
     """
 
     for arg in args:
-        if not isinstance(arg, list) or not len(arg) == 2:
+        if not isinstance(arg, list) \
+                or not len(arg) == 2 \
+                or not isinstance(arg[0], int) \
+                or not isinstance(arg[1], int):
             return False
     return True
 
@@ -35,11 +31,6 @@ def lcm(a, b):
     """
     Least common multiple of a and b.
     Najmniejsza wspólna wielokrotność
-
-    >>> lcm(2, 3)
-    6
-    >>> lcm(21, 6)
-    42
     """
 
     return abs(a * b) // gcd(a, b)
@@ -48,15 +39,6 @@ def lcm(a, b):
 def simplify_frac(frac):
     """
     Upraszcza ułamek.
-
-    >>> simplify_frac([2, 4])
-    [1, 2]
-    >>> simplify_frac([-8, -10])
-    [4, 5]
-    >>> simplify_frac([1, -3])
-    [-1, 3]
-    >>> simplify_frac([0, 4])
-    [0, 1]
     """
 
     if not is_frac(frac):
@@ -73,13 +55,6 @@ def simplify_frac(frac):
 def abs_frac(frac):
     """
     Zwraca wartość bezwzględną ułamka.
-
-    >>> abs_frac([2, 4])
-    [2, 4]
-    >>> abs_frac([-1, 2])
-    [1, 2]
-    >>> abs_frac([1, -2])
-    [1, 2]
     """
 
     if not is_frac(frac):
@@ -91,11 +66,6 @@ def abs_frac(frac):
 def reverse_frac(frac):
     """
     Odwraca zwrócony ułamek.
-
-    >>> reverse_frac([1, 2])
-    [2, 1]
-    >>> reverse_frac([8, -4])
-    [-4, 8]
     """
 
     if not is_frac(frac):
@@ -107,13 +77,6 @@ def reverse_frac(frac):
 def add_frac(frac1, frac2):
     """
     Zwraca ułamek będący sumą frac1 i frac2.
-
-    >>> add_frac([1, 2], [1, 3])
-    [5, 6]
-    >>> add_frac([1, 2], [6, 4])
-    [2, 1]
-    >>> add_frac([-3, 4], [3, 4])
-    [0, 1]
     """
 
     if not is_frac(frac1, frac2):
@@ -128,15 +91,6 @@ def add_frac(frac1, frac2):
 def sub_frac(frac1, frac2):
     """
     Zwraca ułamek będący różnicą frac1 i frac2.
-
-    >>> sub_frac([3, 4], [1, 4])
-    [1, 2]
-    >>> sub_frac([3, 4], [4, 5])
-    [-1, 20]
-    >>> sub_frac([1, 3], [-2, 3])
-    [1, 1]
-    >>> sub_frac([-2, 4], [-3, 4])
-    [1, 4]
     """
 
     if not is_frac(frac1, frac2):
@@ -151,13 +105,6 @@ def sub_frac(frac1, frac2):
 def mul_frac(frac1, frac2):
     """
     Zwraca ułamek będący wynikiem mnożenia frac1 przez frac2.
-
-    >>> mul_frac([3, 4], [1, 2])
-    [3, 8]
-    >>> mul_frac([3, 4], [0, 2])
-    [0, 1]
-    >>> mul_frac([3, 4], [-1, 2])
-    [-3, 8]
     """
 
     if not is_frac(frac1, frac2):
@@ -169,15 +116,6 @@ def mul_frac(frac1, frac2):
 def div_frac(frac1, frac2):
     """
     Zwraca ułamek będący wynikiem dzielenia frac1 przez frac2.
-
-    >>> div_frac([3, 4], [1, 2])
-    [3, 2]
-    >>> div_frac([3, 4], [-1, 2])
-    [-3, 2]
-    >>> div_frac([3, 4], [0, 2])
-    Traceback (most recent call last):
-        ...
-    ValueError: Nie mozna dzielic przez zero
     """
 
     if not is_frac(frac1, frac2):
@@ -191,15 +129,6 @@ def div_frac(frac1, frac2):
 def is_positive(frac):
     """
     Sprawdza czy ułamek ma dodatnią wartość.
-
-    >>> is_positive([1, 2])
-    True
-    >>> is_positive([-1, -2])
-    True
-    >>> is_positive([-1, 2])
-    False
-    >>> is_positive([1, -2])
-    False
     """
 
     if not is_frac(frac):
@@ -212,11 +141,6 @@ def is_positive(frac):
 def is_zero(frac):
     """
     Sprawdza czy ułamek jest równy zero.
-
-    >>> is_zero([0, 3])
-    True
-    >>> is_zero([1, 2])
-    False
     """
 
     if not is_frac(frac):
@@ -230,13 +154,6 @@ def is_zero(frac):
 def cmp_frac(frac1, frac2):
     """
     Porównuje dwa ułamki
-
-    >>> cmp_frac([5,2], [3,2])
-    1
-    >>> cmp_frac([1,2], [3,2])
-    -1
-    >>> cmp_frac([3,2], [3,2])
-    0
     """
 
     if not is_frac(frac1, frac2):
@@ -253,11 +170,6 @@ def cmp_frac(frac1, frac2):
 def frac2float(frac):
     """
     Konwertuje ułamek do float.
-
-    >>> frac2float([1, 2])
-    0.5
-    >>> frac2float([-3, 2])
-    -1.5
     """
 
     if not is_frac(frac):
