3e19045d39954c6b8a93d63d48f9f7b90e95d32a
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-11-12 16:47:06 +0100

    Fracs.py : is_frac(), i sub_frac()

--- lekcja_05/fracs.py
+++ lekcja_05/fracs.py
@@ -11,6 +11,23 @@ import doctest
 from math import gcd    # fractions.gcd is deprecated
 
 
+def is_frac(frac):
+    """
+    Sprawdza czy frac jest ułamkiem i zwraca True lub False.
+
+    >>> is_frac([1, 2])
+    True
+    >>> is_frac([1, 2, 3])
+    False
+    >>> is_frac(5)
+    False
+    """
+
+    if not isinstance(frac, list) or not len(frac) == 2:
+        return False
+    return True
+
+
 def lcm(a, b):
     """
     Least common multiple of a and b.
@@ -39,7 +56,7 @@ def simplify_frac(frac):
     [0, 1]
     """
 
-    if not isinstance(frac, list) or not len(frac) == 2:
+    if not is_frac(frac):
         raise TypeError("frac nie jest ulamkiem")
 
     licznik, mianownik = frac[0], frac[1]
@@ -62,9 +79,9 @@ def add_frac(frac1, frac2):
     [0, 1]
     """
 
-    if not isinstance(frac1, list) or not len(frac1) == 2:
+    if not is_frac(frac1):
         raise TypeError("frac1 nie jest ulamkiem")
-    if not isinstance(frac2, list) or not len(frac2) == 2:
+    if not is_frac(frac2):
         raise TypeError("frac2 nie jest ulamkiem")
 
     mianownik = lcm(frac1[1], frac2[1])
@@ -73,7 +90,30 @@ def add_frac(frac1, frac2):
     return simplify_frac([licznik, mianownik])
 
 
-def sub_frac(frac1, frac2): pass
+def sub_frac(frac1, frac2):
+    """
+    Zwraca ułamek będący różnicą frac1 i frac2.
+
+    >>> sub_frac([3, 4], [1, 4])
+    [1, 2]
+    >>> sub_frac([3, 4], [4, 5])
+    [-1, 20]
+    >>> sub_frac([1, 3], [-2, 3])
+    [1, 1]
+    >>> sub_frac([-2, 4], [-3, 4])
+    [1, 4]
+    """
+
+    if not is_frac(frac1):
+        raise TypeError("frac1 nie jest ulamkiem")
+    if not is_frac(frac2):
+        raise TypeError("frac2 nie jest ulamkiem")
+
+    mianownik = lcm(frac1[1], frac2[1])
+    licznik = frac1[0] * mianownik // frac1[1] - \
+        frac2[0] * mianownik // frac2[1]
+    return simplify_frac([licznik, mianownik])
+    
 
 
 def mul_frac(frac1, frac2): pass
