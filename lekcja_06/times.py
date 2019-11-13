3b66dca5a641e9393fae7a47c17194d699853c5a
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-11-13 10:55:57 +0100

    dodana część testów do Times

---
+++ lekcja_06/times.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+class Time:
+    """Klasa reprezentująca odcinek czasu."""
+
+    def __init__(self, s=0):
+        """Zwraca instancję klasy Time."""
+        self.s = int(s)
+
+    def __str__(self):
+        """Zwraca string 'hh:mm:ss'."""
+        h = self.s // 3600
+        sec = self.s - h * 3600
+        m = sec // 60
+        sec = sec - m * 60
+        return "{0:02d}:{1:02d}:{2:02d}".format(h, m, sec)
+    # 4834 : 01:20:34
+
+    def __repr__(self):
+        """Zwraca string 'Time(s)'."""
+        return "Time({0})".format(self.s)
+
+    def __add__(self, other):
+        """Dodawanie odcinków czasu."""
+        return Time(self.s + other.s)
+
+#   def __cmp__(self, other):           # porównywanie, -1|0|+1
+#       """Porównywanie odcinków czasu."""
+#       return cmp(self.s, other.s)
+
+    def __lt__(self, other):    	# <
+        return self.s < other.s
+
+    def __gt__(self, other):    	# >
+        return self.s > other.s
+
+    def __le__(self, other):    	# <=
+        return self.s <= other.s
+
+    def __ge__(self, other):    	# >=
+        return self.s >= other.s
+
+    def __eq__(self, other):    	# ==
+        return self.s == other.s
+
+    def __ne__(self, other):            # !=
+        return self.s != other.s
+
+    def __int__(self):                  # int(time1)
+        """Konwersja odcinka czasu do int."""
+        return self.s
+
+# Kod testujący moduł.
+
+import unittest
+
+class TestTime(unittest.TestCase):
+
+    def setUp(self): pass
+
+    def test_print(self):               # test str() i repr()
+        self.assertEqual(str(Time(4834)), '01:20:34')
+
+    def test_add(self):
+        self.assertEqual(Time(1) + Time(2), Time(3))
+
+    def test_cmp(self):
+       # Można sprawdzać ==, !=, >, >=, <, <=.
+       self.assertTrue(Time(1) == Time(1))
+       self.assertTrue(Time(1) != Time(2))
+       self.assertTrue(Time(3) > Time(2))
+       self.assertTrue(Time(3) >= Time(3))
+       self.assertTrue(Time(2) < Time(3))
+       self.assertTrue(Time(3) <= Time(3))
+
+    def test_int(self):
+        self.assertEqual(int(Time(10)), 10)
+        self.assertEqual(int(Time(10)), 10)
+
+    def tearDown(self): pass
+
+if __name__ == "__main__":
+    unittest.main()     # wszystkie testy
+
