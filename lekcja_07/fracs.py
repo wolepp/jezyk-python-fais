be955dd1b6594b7fcb2b1a180b647bd5db04b727
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-11-16 18:57:17 +0100

    dodana obsługa działań na ułamkach z liczbami float do Fracs Dodano również odpowiednie testy

--- lekcja_07/fracs.py
+++ lekcja_07/fracs.py
@@ -12,6 +12,8 @@ class Frac:
     """Klasa reprezentująca ułamek."""
 
     def __init__(self, x=0, y=1):
+        if not isinstance(x, int) or not isinstance(y, int):
+            raise TypeError('Licznik i mianownik muszą być typu int')
         if y == 0:
             raise ValueError('Mianownik musi być różny od 0')
 
@@ -42,10 +44,12 @@ class Frac:
         """Najmniejsza wspólna wielokrotność a i b."""
         return abs(a * b) // gcd(a, b)
 
-    def __add__(self, other):  # frac1 + frac2, frac + int
+    def __add__(self, other):  # frac1 + frac2, frac + int, frac + float
         if isinstance(other, int):
             y = self.y
             x = self.x + other * self.y
+        elif isinstance(other, float):
+            return self + Frac(*other.as_integer_ratio())
         else:
             y = Frac._lcm(self.y, other.y)
             x = (self.x * y // self.y) + (other.x * y // other.y)
@@ -53,21 +57,27 @@ class Frac:
 
     __radd__ = __add__
 
-    def __sub__(self, other):  # frac1 - frac2, frac - int
+    def __sub__(self, other):  # frac1 - frac2, frac - int, frac - float
         if isinstance(other, int):
             y = self.y
             x = self.x - other * self.y
+        elif isinstance(other, float):
+            return self - Frac(*other.as_integer_ratio())
         else: 
             y = Frac._lcm(self.y, other.y)
             x = (self.x * y // self.y) - (other.x * y // other.y)
         return Frac(x, y).simplify()
 
-    def __rsub__(self, other):  # int - frac
+    def __rsub__(self, other):  # int - frac, float - frac
+        if isinstance(other, float):
+            return Frac(*other.as_integer_ratio()) - self
         return Frac(self.y * other - self.x, self.y)
 
-    def __mul__(self, other):  # frac1 * frac2, frac * int
+    def __mul__(self, other):  # frac1 * frac2, frac * int, frac * float
         if isinstance(other, Frac):
             return Frac(self.x * other.x, self.y * other.y).simplify()
+        if isinstance(other, float):
+            return self * Frac(*other.as_integer_ratio())
         return Frac(self.x * other, self.y).simplify()
 
     __rmul__ = __mul__
@@ -78,6 +88,8 @@ class Frac:
 
         if isinstance(other, int):
             return self * Frac(1, other)
+        if isinstance(other, float):
+            return self * ~Frac(*other.as_integer_ratio())
         return self * ~other
 
     def __rtruediv__(self, other): # int / frac
@@ -159,10 +171,18 @@ class TestFrac(unittest.TestCase):
         self.assertEqual(Frac(1, 2) + 3, Frac(7, 2))
         self.assertEqual(Frac(1, 2) + (-5), Frac(-9, 2))
 
+        # frac + float
+        self.assertEqual(Frac(1, 2) + 0.5, Frac(1, 1))
+        self.assertEqual(Frac(5, 3) + 1.5, Frac(19, 6))
+
     def test___radd__(self):
         # int + frac
         self.assertEqual(3 + Frac(1, 2), Frac(7, 2))
         self.assertEqual(-5 + Frac(1, 2), Frac(-9, 2))
+
+        # float + frac
+        self.assertEqual(0.5 + Frac(1, 2), Frac(1, 1))
+        self.assertEqual(1.5 + Frac(5, 3), Frac(19, 6))
 
     def test___sub__(self):
         # frac - frac
@@ -175,10 +195,18 @@ class TestFrac(unittest.TestCase):
         self.assertEqual(Frac(3, 4) - 2, Frac(-5, 4))
         self.assertEqual(Frac(1, 2) - 5, Frac(-9, 2))
 
+        # frac - float
+        self.assertEqual(Frac(1, 2) - 0.5, Frac(0, 1))
+        self.assertEqual(Frac(5, 3) - 1.5, Frac(1, 6))
+
     def test___rsub__(self):
         # int - frac
         self.assertEqual(2 - Frac(3, 4), Frac(5, 4))
         self.assertEqual(5 - Frac(1, 2), Frac(9, 2))
+
+        # float - frac
+        self.assertEqual(0.5 - Frac(1, 2), Frac(0, 1))
+        self.assertEqual(1.5 - Frac(5, 3), Frac(-1, 6))
 
     def test___mul__(self):
         # frac * frac
@@ -190,17 +218,41 @@ class TestFrac(unittest.TestCase):
         self.assertEqual(Frac(3, 4) * 2, Frac(3, 2))
         self.assertEqual(Frac(-5, 8) * 3, Frac(-15, 8))
 
+        # frac * float
+        self.assertEqual(Frac(2, 3) * 0.5, Frac(1, 3))
+
     def test___rmul__(self):
         # int * frac
         self.assertEqual(2 * Frac(3, 4), Frac(3, 2))
         self.assertEqual(3 * Frac(-5, 8), Frac(-15, 8))
+        
+        # frac * float
+        self.assertEqual(0.5 * Frac(2, 3), Frac(1, 3))
 
-
-    def test___div__(self):
+    def test___truediv__(self):
+        # frac / frac
         self.assertEqual(Frac(3, 4) / Frac(1, 2), Frac(3, 2))
         self.assertEqual(Frac(3, 4) / Frac(-1, 2), Frac(-3, 2))
 
+        # frac / int
+        self.assertEqual(Frac(3, 4) / 2, Frac(3, 8))
+        self.assertEqual(Frac(3, 4) / (-2), Frac(-3, 8))
+
+        # frac / float
+        self.assertEqual(Frac(3, 2) / 1.5, Frac(1, 1))
+
+        # dzielenie przez zero
         self.assertRaises(ZeroDivisionError, lambda: Frac(1, 3) / Frac(0, 1))
+        self.assertRaises(ZeroDivisionError, lambda: Frac(1, 3) / 0)
+        self.assertRaises(ZeroDivisionError, lambda: Frac(1, 3) / 0.0)
+
+    def test___rtruediv__(self):
+        # int / frac
+        self.assertAlmostEqual(2 / Frac(3, 4), 2.66666666)
+        self.assertAlmostEqual(-2 / Frac(3, 4), -2.66666666)
+
+        # float / frac
+        self.assertEqual(1.5 / Frac(3, 2), Frac(1, 1))
 
     def test___pos__(self):
         self.assertEqual(+Frac(2, 5), Frac(2, 5))
