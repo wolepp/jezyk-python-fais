bf53eb319616a48fa6b19a759571f32ad37a1ed9
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2019-12-07 20:12:09 +0100

    Dodano rozwiązania zadań 9.1 i 9.2. Rozszerzenia klasy singlelist

--- lekcja_09/singlelist.py
+++ lekcja_09/singlelist.py
@@ -50,3 +50,78 @@ class SingleList:
         node.next = None   # czyszczenie łącza
         self.length -= 1
         return node   # zwracamy usuwany node
+
+    def remove_tail(self):          # klasy O(N)
+        """Skraca listę o jej ogon i go zwraca."""
+        if self.length == 0:
+            raise ValueError("pusta lista")
+        tail = self.tail
+        if self.head == self.tail:   # self.length == 1
+            self.head = self.tail = None
+        else:
+            node = self.head
+            while node.next is not self.tail:
+                node = node.next
+            node.next = None
+            self.tail = node
+        return tail
+
+    def merge(self, other):         # klasy O(1)
+        """Węzły z listy other są przepinane do listy self na jej koniec."""
+        if other.length == 0:
+            return
+        if self.length == 0:
+            self.head = other.head
+        else:
+            self.tail.next = other.head
+        self.tail = other.tail
+        self.length += other.count()
+
+    def clear(self):            # czyszczenie listy
+        """Czyści listę."""
+        self.head = self.tail = None
+        self.length = 0
+
+    def search(self, data):   # klasy O(N)
+        """Zwraca łącze do węzła o podanym kluczu lub None."""
+        if self.length == 0:
+            return None
+        node = self.head
+        while node.data != data:
+            if node.next is None:
+                return None
+            node = node.next
+        return node
+
+    def find_min(self):         # klasy O(N)
+        """Zwraca łącze do węzła z najmniejszym kluczem."""
+        if self.length == 0:
+            return None
+        min_node = node = self.head
+        while node.next:
+            node = node.next
+            if node.data < min_node.data:
+                min_node = node
+        return min_node
+
+    def find_max(self):         # klasy O(N)
+        """Zwraca łącze do węzła z największym kluczem."""
+        if self.length == 0:
+            return None
+        max_node = node = self.head
+        while node.next:
+            node = node.next
+            if node.data > max_node.data:
+                max_node = node
+        return max_node
+
+    def reverse(self):          # klasy O(N)
+        """Odwracanie kolejności węzłów na liście."""
+        current = self.head
+        prev_node = None
+        while current is not None:
+            next_node = current.next
+            current.next = prev_node
+            prev_node = current
+            current = next_node
+        self.head, self.tail = self.tail, self.head
