3ce9e5fc4fdb7a4363e9b62382e1a6e050edba0b
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-01-11 20:38:04 +0100

    Dodany oneliner (dla sztuki) do wyszukiwania liniowego.

--- lekcja_12/wysz_liniowe.py
+++ lekcja_12/wysz_liniowe.py
@@ -8,16 +8,20 @@ wystąpienia przy pomocy wyszukiwania liniowego.
 
 import random
 
-
 if __name__ == "__main__":
-    n, k = 100, 10
+    n = 100
+    k = 10
     L = [random.randrange(0, k) for i in range(n)]
 
     y = random.randrange(0, k)
+
     wystapienia = []
     for (indeks, liczba) in enumerate(L):
         if liczba == y:
             wystapienia.append(indeks)
 
+    # wystapienia = [i[0] for i in (filter(lambda idx_num: idx_num[1] == y, enumerate(L)))]
+
     print('{} jest na pozycjach: {}'.format(y, wystapienia))
-    print(L)
+    print('Lista z pozycjami (indeks, liczba):')
+    print(list(enumerate(L)))
