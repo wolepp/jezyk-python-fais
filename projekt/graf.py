afd4000106ec2879d14feed5081d5a39408d49e6
Wojciech Lepich <wojciech.lepich@student.uj.edu.pl>
2020-01-14 19:04:58 +0100

    Pierwsza wersja projektu

--- projekt/graf.py
+++ projekt/graf.py
@@ -3,26 +3,55 @@ Algorytm Floyda-Warshalla.
 
 Wojciech Lepich
 """
+from math import inf
 import graphutil
 
 def floydwarshall(graph):
-    n = len(graphutil.list_nodes(graph))
+    nodes = graphutil.list_nodes(graph)
+    edges = graphutil.list_edges(graph)
+
+    # ilość wierzchołków
+    n = len(nodes)
+
+    # macierz d[n*n], każda wartość zainicjalizowana wartością nieskończoność
+    # w postaci słownika
+    d = {}
+    for source in nodes:
+        d[source] = {}
+        for target in nodes:
+            d[source][target] = inf if source != target else 0
+
+    # uzupełnienie wag przejścia między wierzchołkami o krawędzie
+    for edge in edges:
+        source, target, weight = edge
+        d[source][target] = weight
+
+    for middle in nodes:
+        for source in nodes:
+            for target in nodes:
+                new_weight = d[source][middle] + d[middle][target]
+                if d[source][target] > new_weight:
+                    d[source][target] = new_weight
+
+    return d
 
 if __name__ == "__main__":
 
     graph = {
         "A": {"B": 1, "C": 2},
-        "B": {"C": 3, "D": 4},
-        "C": {"D": 5},
+        "B": {"C": 3, "D": 5, "F": 8},
+        "C": {"A": 3, "D": 1},
         "D": {"C": 6},
         "E": {"C": 7},
-        "F": {}
+        "F": {"B": 1, "E": 3}
     }
 
-    graphutil.add_node(graph, "G")
-    graphutil.add_edge_directed(graph, ('G', 'C', 9))
+    # d = floydwarshall(graph)
+    # graphutil.print_graph(d)
 
-    graphutil.print_graph(graph)
-    print(graphutil.list_edges(graph))
-    print(graphutil.list_nodes(graph))
-    print('wierzchołków:', len(graphutil.list_nodes(graph)))
+    randomowy = graphutil.random_graph(4, 30)
+    floyd = floydwarshall(randomowy)
+    graphutil.print_graph(randomowy)
+    print()
+    graphutil.print_graph(floyd)
+    
